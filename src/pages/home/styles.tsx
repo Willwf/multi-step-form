7c24ebe3371c842ede902b0ad37f5090d36bb133
William Firmino <williamwf07@gmail.com>
2023-04-24 14:30:41 -0300

    feat: style the submit button of the form

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -1,7 +1,31 @@
 import styled from "styled-components";
+import * as Styles from "../../styles/variables";
 
 export const FormDiv = styled.div``;
 
-export const NextButtonDiv = styled.div``;
+export const NextButtonDiv = styled.div`
+  width: 100vw;
+  min-height: 10vh;
+  background-color: ${Styles.white};
 
-export const NextButton = styled.button``;
+  position: absolute;
+  bottom: 0;
+
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+
+export const NextButton = styled.button`
+  height: 5vh;
+  width: 25vw;
+  cursor: pointer;
+
+  font-weight: 500;
+  color: ${Styles.magnolia};
+
+  margin-right: 5vw;
+
+  border-radius: 0.5rem;
+  background-color: ${Styles.marineBlue};
+`;
