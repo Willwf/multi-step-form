0ecf4a04299ad91d38ab20bd90df223762e64ad0
William Firmino <williamwf07@gmail.com>
2023-04-28 18:10:12 -0300

    feat: create plan option element

--- src/components/select-your-plan-form/index.tsx
+++ src/components/select-your-plan-form/index.tsx
@@ -1,5 +1,9 @@
 import * as Styles from "./styles";
 
+import arcade from "../../assets/images/icon-arcade.svg";
+import advanced from "../../assets/images/icon-advanced.svg";
+import pro from "../../assets/images/icon-pro.svg";
+
 export function SelectYourPlan() {
   return (
     <Styles.Main>
@@ -11,6 +15,29 @@ export function SelectYourPlan() {
               You have the option of monthly or yearly billing.
             </Styles.FormSubtitle>
           </Styles.FormTitleDiv>
+          <Styles.PlanOptionDiv className="selected">
+            <img src={arcade} />
+            <Styles.PlanOptionInfoDiv>
+              <Styles.PlanOptionTitle>Arcade</Styles.PlanOptionTitle>
+              <Styles.PlanOptionPrice>$9/mo</Styles.PlanOptionPrice>
+            </Styles.PlanOptionInfoDiv>
+          </Styles.PlanOptionDiv>
+
+          <Styles.PlanOptionDiv>
+            <img src={advanced} />
+            <Styles.PlanOptionInfoDiv>
+              <Styles.PlanOptionTitle>Advanced</Styles.PlanOptionTitle>
+              <Styles.PlanOptionPrice>$12/mo</Styles.PlanOptionPrice>
+            </Styles.PlanOptionInfoDiv>
+          </Styles.PlanOptionDiv>
+
+          <Styles.PlanOptionDiv>
+            <img src={pro} />
+            <Styles.PlanOptionInfoDiv>
+              <Styles.PlanOptionTitle>Pro</Styles.PlanOptionTitle>
+              <Styles.PlanOptionPrice>$15/mo</Styles.PlanOptionPrice>
+            </Styles.PlanOptionInfoDiv>
+          </Styles.PlanOptionDiv>
         </Styles.Form>
       </Styles.FormSection>
     </Styles.Main>
--- src/components/select-your-plan-form/styles.tsx
+++ src/components/select-your-plan-form/styles.tsx
@@ -19,7 +19,12 @@ export const Main = styled.main`
 `;
 
 export const FormSection = styled.section``;
-export const Form = styled.form``;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 export const FormTitleDiv = styled.div`
   display: flex;
@@ -38,3 +43,47 @@ export const FormSubtitle = styled.p`
   line-height: 2.5rem;
   font-weight: 400;
 `;
+
+export const PlanOptionDiv = styled.div`
+  border: 1px solid ${Styles.lightGray};
+  border-radius: 0.7rem;
+
+  padding: 1.5rem;
+
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+
+  & img {
+    width: 14%;
+  }
+
+  &.selected {
+    border-color: ${Styles.marineBlue};
+    background-color: ${Styles.alabaster};
+  }
+`;
+
+export const PlanOptionInfoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+`;
+
+export const PlanOptionTitle = styled.p`
+  font-size: 1.6rem;
+  font-weight: 700;
+  color: ${Styles.marineBlue};
+`;
+
+export const PlanOptionPrice = styled.p`
+  font-size: 1.3rem;
+  color: ${Styles.coolGray};
+`;
+
+export const PlanOptionGift = styled.p`
+  font-size: 1.2rem;
+  color: ${Styles.marineBlue};
+`;
+
+export const PlanToggleDiv = styled.div``;
