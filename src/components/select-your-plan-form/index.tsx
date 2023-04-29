1ab09b3a1bd299a428f1dd7139ef1ee8523eebbe
William Firmino <williamwf07@gmail.com>
2023-04-29 04:15:32 -0300

    feat: create payment period toggle button

--- src/components/select-your-plan-form/index.tsx
+++ src/components/select-your-plan-form/index.tsx
@@ -3,8 +3,17 @@ import * as Styles from "./styles";
 import arcade from "../../assets/images/icon-arcade.svg";
 import advanced from "../../assets/images/icon-advanced.svg";
 import pro from "../../assets/images/icon-pro.svg";
+import { useState } from "react";
 
 export function SelectYourPlan() {
+  const [isPaymentYearly, setIsPaymentYearly] = useState(false);
+
+  function switchToggle() {
+    setIsPaymentYearly(!isPaymentYearly);
+  }
+
+  console.log(isPaymentYearly);
+
   return (
     <Styles.Main>
       <Styles.FormSection>
@@ -38,6 +47,24 @@ export function SelectYourPlan() {
               <Styles.PlanOptionPrice>$15/mo</Styles.PlanOptionPrice>
             </Styles.PlanOptionInfoDiv>
           </Styles.PlanOptionDiv>
+
+          <Styles.PlanToggleDiv>
+            <Styles.PlanTogglePeriod
+              className={isPaymentYearly ? "" : "active"}
+            >
+              Monthly
+            </Styles.PlanTogglePeriod>
+            <Styles.PeriodToggleDiv onClick={switchToggle}>
+              <Styles.SelectorPeriodToggleDiv
+                className={isPaymentYearly ? "active" : ""}
+              ></Styles.SelectorPeriodToggleDiv>
+            </Styles.PeriodToggleDiv>
+            <Styles.PlanTogglePeriod
+              className={isPaymentYearly ? "active" : ""}
+            >
+              Yearly
+            </Styles.PlanTogglePeriod>
+          </Styles.PlanToggleDiv>
         </Styles.Form>
       </Styles.FormSection>
     </Styles.Main>
--- src/components/select-your-plan-form/styles.tsx
+++ src/components/select-your-plan-form/styles.tsx
@@ -86,4 +86,52 @@ export const PlanOptionGift = styled.p`
   color: ${Styles.marineBlue};
 `;
 
-export const PlanToggleDiv = styled.div``;
+export const PlanToggleDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+
+  margin-top: 1.5rem;
+  border-radius: 0.7rem;
+  background-color: ${Styles.alabaster};
+
+  padding: 1.4rem;
+`;
+
+export const PlanTogglePeriod = styled.p`
+  font-size: 1.4rem;
+  font-weight: 700;
+  color: ${Styles.coolGray};
+
+  &.active {
+    color: ${Styles.marineBlue};
+  }
+`;
+
+export const PeriodToggleDiv = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 4rem;
+  height: 2rem;
+
+  padding: 0.4rem;
+
+  background-color: ${Styles.marineBlue};
+  border-radius: 2rem;
+`;
+
+export const SelectorPeriodToggleDiv = styled.div`
+  width: 1.2rem;
+  height: 1.2rem;
+
+  background-color: ${Styles.white};
+  border-radius: 100%;
+
+  transition: all 0.1s ease-out;
+
+  &.active {
+    transform: translate(2rem);
+  }
+`;
