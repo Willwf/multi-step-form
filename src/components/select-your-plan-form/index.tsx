b50445cd282eb495e16488bee0851b3087ee68b9
William Firmino <williamwf07@gmail.com>
2023-05-01 12:04:22 -0300

    feat: go to the next or back step using the buttons

--- src/components/select-your-plan-form/index.tsx
+++ src/components/select-your-plan-form/index.tsx
@@ -5,68 +5,66 @@ import advanced from "../../assets/images/icon-advanced.svg";
 import pro from "../../assets/images/icon-pro.svg";
 import { useState } from "react";
 
-export function SelectYourPlan() {
+interface SelectYourPlanProps {
+  className: string;
+}
+
+export function SelectYourPlan(props: SelectYourPlanProps) {
+  const { className } = props;
+
   const [isPaymentYearly, setIsPaymentYearly] = useState(false);
 
   function switchToggle() {
     setIsPaymentYearly(!isPaymentYearly);
   }
 
-  console.log(isPaymentYearly);
-
   return (
-    <Styles.Main>
-      <Styles.FormSection>
-        <Styles.Form>
-          <Styles.FormTitleDiv>
-            <Styles.FormTitle>Select your plan</Styles.FormTitle>
-            <Styles.FormSubtitle>
-              You have the option of monthly or yearly billing.
-            </Styles.FormSubtitle>
-          </Styles.FormTitleDiv>
-          <Styles.PlanOptionDiv className="selected">
-            <img src={arcade} />
-            <Styles.PlanOptionInfoDiv>
-              <Styles.PlanOptionTitle>Arcade</Styles.PlanOptionTitle>
-              <Styles.PlanOptionPrice>$9/mo</Styles.PlanOptionPrice>
-            </Styles.PlanOptionInfoDiv>
-          </Styles.PlanOptionDiv>
+    <Styles.FormSection className={className}>
+      <Styles.FormSectionDiv>
+        <Styles.FormTitleDiv>
+          <Styles.FormTitle>Select your plan</Styles.FormTitle>
+          <Styles.FormSubtitle>
+            You have the option of monthly or yearly billing.
+          </Styles.FormSubtitle>
+        </Styles.FormTitleDiv>
+        <Styles.PlanOptionDiv className="selected">
+          <img src={arcade} />
+          <Styles.PlanOptionInfoDiv>
+            <Styles.PlanOptionTitle>Arcade</Styles.PlanOptionTitle>
+            <Styles.PlanOptionPrice>$9/mo</Styles.PlanOptionPrice>
+          </Styles.PlanOptionInfoDiv>
+        </Styles.PlanOptionDiv>
 
-          <Styles.PlanOptionDiv>
-            <img src={advanced} />
-            <Styles.PlanOptionInfoDiv>
-              <Styles.PlanOptionTitle>Advanced</Styles.PlanOptionTitle>
-              <Styles.PlanOptionPrice>$12/mo</Styles.PlanOptionPrice>
-            </Styles.PlanOptionInfoDiv>
-          </Styles.PlanOptionDiv>
+        <Styles.PlanOptionDiv>
+          <img src={advanced} />
+          <Styles.PlanOptionInfoDiv>
+            <Styles.PlanOptionTitle>Advanced</Styles.PlanOptionTitle>
+            <Styles.PlanOptionPrice>$12/mo</Styles.PlanOptionPrice>
+          </Styles.PlanOptionInfoDiv>
+        </Styles.PlanOptionDiv>
 
-          <Styles.PlanOptionDiv>
-            <img src={pro} />
-            <Styles.PlanOptionInfoDiv>
-              <Styles.PlanOptionTitle>Pro</Styles.PlanOptionTitle>
-              <Styles.PlanOptionPrice>$15/mo</Styles.PlanOptionPrice>
-            </Styles.PlanOptionInfoDiv>
-          </Styles.PlanOptionDiv>
+        <Styles.PlanOptionDiv>
+          <img src={pro} />
+          <Styles.PlanOptionInfoDiv>
+            <Styles.PlanOptionTitle>Pro</Styles.PlanOptionTitle>
+            <Styles.PlanOptionPrice>$15/mo</Styles.PlanOptionPrice>
+          </Styles.PlanOptionInfoDiv>
+        </Styles.PlanOptionDiv>
 
-          <Styles.PlanToggleDiv>
-            <Styles.PlanTogglePeriod
-              className={isPaymentYearly ? "" : "active"}
-            >
-              Monthly
-            </Styles.PlanTogglePeriod>
-            <Styles.PeriodToggleDiv onClick={switchToggle}>
-              <Styles.SelectorPeriodToggleDiv
-                className={isPaymentYearly ? "active" : ""}
-              ></Styles.SelectorPeriodToggleDiv>
-            </Styles.PeriodToggleDiv>
-            <Styles.PlanTogglePeriod
+        <Styles.PlanToggleDiv>
+          <Styles.PlanTogglePeriod className={isPaymentYearly ? "" : "active"}>
+            Monthly
+          </Styles.PlanTogglePeriod>
+          <Styles.PeriodToggleDiv onClick={switchToggle}>
+            <Styles.SelectorPeriodToggleDiv
               className={isPaymentYearly ? "active" : ""}
-            >
-              Yearly
-            </Styles.PlanTogglePeriod>
-          </Styles.PlanToggleDiv>
-        </Styles.Form>
-      </Styles.FormSection>
-    </Styles.Main>
+            ></Styles.SelectorPeriodToggleDiv>
+          </Styles.PeriodToggleDiv>
+          <Styles.PlanTogglePeriod className={isPaymentYearly ? "active" : ""}>
+            Yearly
+          </Styles.PlanTogglePeriod>
+        </Styles.PlanToggleDiv>
+      </Styles.FormSectionDiv>
+    </Styles.FormSection>
   );
 }
--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -1,19 +1,65 @@
 import styled from "styled-components";
 import * as Styles from "../../styles/variables";
 
-export const FormDiv = styled.div``;
+export const Form = styled.form``;
+
+export const FormSectionsDiv = styled.div`
+  width: 90vw;
+  min-height: 40vh;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  top: 11vh;
+
+  background-color: ${Styles.white};
+
+  padding: 3rem 2rem;
+  border-radius: 1rem;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.2);
+`;
 
 export const NextButtonDiv = styled.div`
   width: 100vw;
   min-height: 10vh;
   background-color: ${Styles.white};
 
-  position: absolute;
+  position: fixed;
   bottom: 0;
 
   display: flex;
   justify-content: end;
   align-items: center;
+
+  &.backButtonActive {
+    justify-content: space-between;
+  }
+`;
+
+export const BackButton = styled.button`
+  height: 5vh;
+  width: 25vw;
+  cursor: pointer;
+
+  font-weight: 500;
+  color: ${Styles.coolGray};
+
+  margin-left: 5vw;
+
+  border: none;
+  border-radius: 0.5rem;
+  background-color: transparent;
+
+  display: none;
+
+  &:hover {
+    background-color: ${Styles.magnolia};
+  }
+
+  &.active {
+    display: inline;
+  }
 `;
 
 export const NextButton = styled.button`
@@ -22,7 +68,7 @@ export const NextButton = styled.button`
   cursor: pointer;
 
   font-weight: 500;
-  color: ${Styles.magnolia};
+  color: ${Styles.lightGray};
 
   margin-right: 5vw;
 
